3afdb8e02511e9a1d720d8608a7032dc55440eb6
Alexis Rondeau <alexis.rondeau@gmail.com>
2022-11-19 15:01:15 +0100

    UPdates lines and type

--- Streamline/Views/RageTextInputView.swift
+++ Streamline/Views/RageTextInputView.swift
@@ -11,47 +11,48 @@ import SwiftUIX
 struct RageTextInputView: View {
     @EnvironmentObject var state: AppState
     @State private var opacity = 1.0
-
+    
     var body: some View {
         GeometryReader { geometry in
             Path() { path in
-                    path.move(to: CGPoint(x: 0, y: state.calculatedFontSize() * 1.5))
-                    path.addLine(to: CGPoint(x: geometry.size.width, y: state.calculatedFontSize() * 1.5))
-                }
-                .stroke(Color("LineColor"), lineWidth: 1)
-                .opacity(0.1)
-
-                HStack {
-                    Text(state.attributedString)
-                        .font(.system(size: state.calculatedFontSize()))
-                        .truncationMode(.head)
-                        .lineLimit(1)
-                        .foregroundColor(Color("TextColor"))
-                        .frame(width: geometry.size.width * state.ratioLeft, alignment: .trailing)
-                        .mask(LinearGradient(gradient: Gradient(colors: [.clear, Color("MaskColor")]), startPoint: .leading, endPoint: .trailing))
-                        .offset(x: -15)
-
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 3, style: .continuous)
-                            .fill(Color("CursorColor"))
-                            .frame(width: state.calculatedFontSize() / 2.5, height: state.calculatedFontSize() * 1.5)
-                            .opacity(opacity)
-                            .onAppear() {
-                                withAnimation(.easeInOut(duration: 2).repeatForever()) {
-                                    opacity = 0.2
-                                }
+                path.move(to: CGPoint(x: geometry.size.width * state.ratioLeft, y: -geometry.size.height))
+                path.addLine(to: CGPoint(x: geometry.size.width * state.ratioLeft, y: geometry.size.height))
+            }
+            .stroke(Color("LineColor"), lineWidth: 1)
+            .opacity(0.1)
+            
+            HStack {
+                Text(state.attributedString)
+                    .font(.system(size: state.calculatedFontSize()))
+                    .truncationMode(.head)
+                    .lineLimit(1)
+                    .foregroundColor(Color("TextColor"))
+                    .frame(width: geometry.size.width * state.ratioLeft, alignment: .trailing)
+                    .mask(LinearGradient(gradient: Gradient(colors: [.clear, Color("MaskColor")]), startPoint: .leading, endPoint: .trailing))
+                    .offset(x: -8, y: -1)
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 0, style: .continuous)
+                        .fill(Color("CursorColor"))
+                        .frame(width: 5, height: state.calculatedFontSize() * 1.5)
+                        .opacity(opacity)
+                        .onAppear() {
+                            withAnimation(.easeInOut(duration: 2).repeatForever()) {
+                                opacity = 0.2
                             }
-                            .offset(x: -23)
-                        
-                        if(state.umlautModifierTyped) {
-                            Text("¨")
-                                .font(.system(size: state.calculatedFontSize()))
-                                .foregroundColor(Color("TextColor"))
-                                .offset(x: -23, y: -(state.calculatedFontSize() / 2 + 5))
                         }
-                    }
+                        .offset(x: -13)
+                }
+                
+                if(state.umlautModifierTyped) {
+                    Text("¨")
+                        .font(.system(size: state.calculatedFontSize()))
+                        .foregroundColor(Color("TextColor"))
+                        .offset(x: -52, y: -(state.calculatedFontSize() / 2 + 5))
                 }
 
+            }
+            
             // Autocomplete
             AutocompleteView()
                 .visible(state.currentlySearching)
